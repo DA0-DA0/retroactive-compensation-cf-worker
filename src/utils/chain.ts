52a8075f5d66d1f53f0aab5c3a961b5f81a5e7d6
Noah Saso <noahsaso@gmail.com>
2024-03-01 14:01:36 -0800

    remove rpc preferences

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -1,30 +1,18 @@
 import { CosmWasmClient } from '@cosmjs/cosmwasm-stargate'
 import { chains } from 'chain-registry'
 
-export const RPC_FOR_CHAIN_ID: Record<string, string | undefined> = {
-  'juno-1': 'https://juno-rpc.reece.sh:443',
-  'uni-6': 'https://rpc.uni.junonetwork.io:443',
-  'osmosis-1': 'https://rpc.osmosis.zone:443',
-  'osmo-test-5': 'https://rpc.testnet.osmosis.zone:443',
-  'stargaze-1': 'https://rpc.stargaze-apis.com',
-  'elgafar-1': 'https://rpc.elgafar-1.stargaze-apis.com',
-  'neutron-1': 'https://rpc-kralum.neutron-1.neutron.org',
-  'migaloo-1': 'https://migaloo-rpc.polkachu.com',
-  'narwhal-2': 'https://migaloo-testnet-rpc.polkachu.com',
-}
 export const getCosmWasmClientForChain = async (
   chainId: string
 ): Promise<CosmWasmClient> => {
   const chain = chains.find((chain) => chain.chain_id === chainId)
-  const rpcs = [
-    RPC_FOR_CHAIN_ID[chainId],
-    ...(chain
+  const rpcs = (
+    chain
       ? [
           `https://rpc.cosmos.directory/${chain.chain_name}`,
           ...(chain.apis?.rpc?.map(({ address }) => address) || []),
         ]
-      : []),
-  ].filter((rpc): rpc is string => !!rpc)
+      : []
+  ).filter((rpc): rpc is string => !!rpc)
   if (!rpcs.length) {
     throw new Error(`No RPC for chain ${chainId}`)
   }
